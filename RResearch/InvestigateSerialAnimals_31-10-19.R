f688245478df3af2894c280f3949e838d9e98b61
Viktor <viktor.perets@ucdconnect.ie>
2019-11-04 08:32:26 +0000

    Made initial plots

--- RResearch/InvestigateSerialAnimals_31-10-19.R
+++ RResearch/InvestigateSerialAnimals_31-10-19.R
@@ -41,10 +41,29 @@ animalOneAlleleIndexes <- which(animalOneUniques > 1)
 animalTwoAlleleIndexes <- which(animalTwoUniques > 1)
 
 # Plot a plot for animal one
+plot(x=NA, y=NA, xaxt = "n", xlim = c(as.Date("2016-09-01"), as.Date("2017-11-01")), ylim = c(0,6), main = "Troublesome Animal One", 
+     ylab = "SNP difference", xlab = "Time", las = 2)
+axis.Date(side = 1, x=c("2016-09-01", "2017-11-01"), format = "%Y-%m")
+lines(x=c(as.Date("2016-09-01"),as.Date("2017-11-01")) , y = c(0,0), col = "purple")
+lines(x=c(as.Date("2017-09-04"),as.Date("2017-09-04")) , y = c(0,5), col = "red")
+lines(x=c(as.Date("2017-09-04"),as.Date("2017-11-01")) , y = c(5,5), col = "red")
+points(x=c(as.Date("2016-10-07"),as.Date("2017-09-04")), y=c(0,5), 
+       col=c("blue","red"), cex = 2, pch = 19)
+text(x=c(as.Date("2016-10-07"),as.Date("2017-09-04")), y = c(0,5),
+     labels = c("16-5561", "17-5559"), cex = 0.7, pos = 3)
 
 # Plot a plot for animal two
-plot(x=NA, y=NA, xaxt = "n", xlim = c(), ylim = c(0,2), main = "Troublesome Animal Two", 
-     ylab = "SNP difference", las = 2)
+plot(x=NA, y=NA, xaxt = "n", xlim = c(as.Date("2016-02-01"), as.Date("2016-03-10")), ylim = c(0,3), main = "Troublesome Animal Two", 
+     ylab = "SNP difference", xlab = "Time", las = 2)
+axis.Date(side = 1, x=c("2016-02-01", "2016-03-10"), format = "%Y-%m-%d")
+lines(x=c(as.Date("2016-02-01"),as.Date("2016-03-10")) , y = c(0,0), col = "purple")
+lines(x=c(as.Date("2016-03-03"),as.Date("2016-03-03")) , y = c(0,2), col = "red")
+lines(x=c(as.Date("2016-03-03"),as.Date("2016-03-10")) , y = c(2,2), col = "red")
+points(x=c(as.Date("2016-02-02"),as.Date("2016-03-03"),as.Date("2016-03-03")), y=c(0,0,2), 
+       col=c("blue","purple","red"), cex = 2, pch = 19)
+text(x=c(as.Date("2016-02-02"),as.Date("2016-03-03"),as.Date("2016-03-03")), y = c(0,0,2),
+     labels = c("16-666", "16-1298 Faeces", "16-1297 Ileocaecal"), cex = 0.7, pos = 3)
+
 
 
 
